6d4c7a38151a6c4e2bb6d615560774041ba4f25d
Chronillogical-Potato <>
2025-12-12 01:07:24 +0000

    Combining Conditions

--- conditional_statements/combining_conditions/main.py
+++ conditional_statements/combining_conditions/main.py
@@ -1,7 +1,9 @@
 # The item's discount and stock status have been defined
 discounted = False
 lowStock = True
+#Check if either is true
 movingProduct = discounted or lowStock
+#Check if not discounted and not lowStock
 promotion = not discounted and not lowStock
 
 print(f"Is the item eligible for promotion? {promotion}.")

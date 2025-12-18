672d6177598be639a76273d04dbb59ca9f001c3e
Chronillogical-Potato <>
2025-12-18 22:41:36 +0000

    Challenge: List Management

--- other_data_types/challenge_list_management/main.py
+++ other_data_types/challenge_list_management/main.py
@@ -3,19 +3,19 @@ meat = ["Ham", 3.99, 50, "Sliced"]
 cheese = ["Cheddar", 5.49, 100, "Sharp"]
 condiment = ["Mustard", 1.99, 75, "Spicy"]
 #2 master list
-deli_debt = [meat, cheese, condiment]
+deli_dept = [meat, cheese, condiment]
 # 1st output req
-print("Initial Deli List: ", deli_debt,".")
+print("Initial Deli List:", deli_dept,".")
 #
 #3
 if ("Ham" in meat) and (meat[2] <= 100):
     meat.pop(2) and meat.insert(2,100)      
 #4 added list
 seasonal_meat = ["Turkey", 4.50, 100, "Sliced"]
-deli_debt.append(seasonal_meat)
+deli_dept.append(seasonal_meat)
 #5 remove condiment
-deli_debt.remove(condiment)
+deli_dept.remove(condiment)
 #6
-deli_debt.sort()
+deli_dept.sort()
 #2nd output req
-print("Updated Deli List:", deli_debt,".")
+print("Updated Deli List:", deli_dept,".")

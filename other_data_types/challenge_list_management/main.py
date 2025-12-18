fb5e78250dbece16c6554aae8fd01cb057076914
Chronillogical-Potato <>
2025-12-18 20:00:55 +0000

    Challenge: List Management

--- other_data_types/challenge_list_management/main.py
+++ other_data_types/challenge_list_management/main.py
@@ -1,12 +1,18 @@
-#inital list
+#1 inital list
 meat = ["Ham", 3.99, 50, "Sliced"]
 cheese = ["Cheddar", 5.49, 100, "Sharp"]
 condiment = ["Mustard", 1.99, 75, "Spicy"]
-#master list
+#2 master list
 deli_debt = [meat, cheese, condiment]
-
+#3
 if ("Ham" in meat) and (meat[2] <= 100):
-    
-#added list
+else:
+    ((meat.pop(2)) and (meat.insert(2,100))
+      
+#4 added list
 seasonal_meat = ["Turkey", 4.50, 100, "Sliced"]
-
+deli_debt.append(seasonal_meat)
+#5 remove condiment
+deli_debt.remove(condiment)
+#6
+deli_debt.sort

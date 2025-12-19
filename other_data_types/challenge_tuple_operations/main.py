4fbb03145d3e90a51b0a8d2d08629846de218833
Chronillogical-Potato <>
2025-12-19 18:48:29 +0000

    Challenge: Tuple Operations

---
+++ other_data_types/challenge_tuple_operations/main.py
@@ -0,0 +1,25 @@
+# Current inventory on shelf
+shelf = ("apples", "oranges", "bananas", "apples", "grapes", "bananas", "apples")
+#variables
+apple_count = shelf.count("apples")
+banana_index = shelf.index("bananas")
+orange_index = shelf.index("oranges")
+grape_count = shelf.count("grapes")
+# apple logic
+if apple_count < 5:
+    print("Apples need to be restocked.")
+else:
+    print("Apples are sufficiently stocked.")
+# grape logic
+if grape_count >= 1:
+    print("Grapes need to be restocked.")
+else:
+    print("Grapes are sufficiently stocked.")
+#orange logic
+if orange_index >= 1:
+    print("Oranges are at index:",orange_index)
+else:
+    print("Oranges are out of stock.")
+#output
+print("Number of Apples:", apple_count)
+print("First Banana Index:",banana_index)
